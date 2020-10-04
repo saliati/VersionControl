6188071e15f4ec32669514521e745023408dad83
Attila Salamon <attila.salamon@stud.uni-corvinus.hu>
2020-10-04 23:00:53 +0200

    FinalChanges

--- ExcelExportalas04/ExcelExportalas04/Form1.cs
+++ ExcelExportalas04/ExcelExportalas04/Form1.cs
@@ -27,8 +27,8 @@ namespace ExcelExportalas04
         {
             InitializeComponent();
             LoadData();
-            //CreateExcel();
-            
+            CreateExcel();
+            FormatTable();
 
 
         }
@@ -38,7 +38,7 @@ namespace ExcelExportalas04
             Flats = context.Flats.ToList();
         }
 
-        /*private void CreateExcel()
+        private void CreateExcel()
         {
            try
               {
@@ -68,12 +68,10 @@ namespace ExcelExportalas04
                     xlWB = null;  //Excel munkafüzet
                     xlApp = null;
                 }  
-        }*/
+        }
 
-        private void CreateTable() //munkalap beállításai
-        {
-            string[] headers = new string[]
-            {
+        string[] headers = new string[]
+           {
                  "Kód",
                  "Eladó",
                  "Oldal",
@@ -83,7 +81,11 @@ namespace ExcelExportalas04
                  "Alapterület (m2)",
                  "Ár (mFt)",
                  "Négyzetméter ár (Ft/m2)"
-            };
+           };
+
+        private void CreateTable() //munkalap beállításai
+        {
+           
 
             for (int i = 0; i < 9; i++)
             {
@@ -91,6 +93,54 @@ namespace ExcelExportalas04
             }
 
             object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+            xlSheet.get_Range(       //??értelmezni
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
     }
 }
