01ee880e3b4cce7f20f846bac9c594d9c33ad696
Attila Salamon <attila.salamon@stud.uni-corvinus.hu>
2020-10-18 00:43:56 +0200

    third

--- SOAP/SOAP/SOAP/Form1.cs
+++ SOAP/SOAP/SOAP/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SOAP.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,25 @@ namespace SOAP
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() //form1 konstruktora
         {
             InitializeComponent();
+
+            //2020-as év első félévének euró árfolyamának lekérdezése
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
         }
     }
 }
