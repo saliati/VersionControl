372221a4ad707471697c894064e2823441771b0e
Attila Salamon <attila.salamon@stud.uni-corvinus.hu>
2020-10-25 23:59:43 +0100

    function

--- Simulation/Microsimulation/Microsimulation/Form1.cs
+++ Simulation/Microsimulation/Microsimulation/Form1.cs
@@ -14,19 +14,46 @@ namespace Microsimulation
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> birthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+
+        Random rnd = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
-
-            List<Person> Population = new List<Person>();
-            List<BirthProbability> birthProbabilities = new List<BirthProbability>();
-            List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+            //Ide kell megadni a két paraméter; év és person
+            //SimStep();
 
             Population = GetPopulation(@"C:\Temp\nép.csv");
             birthProbabilities = GetbirthProbabilities(@"C:\Temp\születés.csv");
             deathProbabilities = GetdeathProbabilities(@"C:\Temp\halál.csv");
 
             dataGridView1.DataSource = Population;
+
+            
+
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+                // kis LINQ
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                //Console.WriteLine - de rég volt ez is :D
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+
         }
 
         private List<Person> GetPopulation(string csvpath)
@@ -92,6 +119,40 @@ namespace Microsimulation
             return deathProbabilities;
         }
 
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
 
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in deathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            // Meghal a személy?
+            if (rnd.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in birthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                //Születik gyermek?
+                if (rnd.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rnd.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
     }
 }
