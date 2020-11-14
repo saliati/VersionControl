3c2d4ce2b5234a1b809a2fb8d61e9e35155253f9
Attila Salamon <attila.salamon@stud.uni-corvinus.hu>
2020-11-15 00:35:34 +0100

    labdagyar

---
+++ Christmas/Christmas/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christmas.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew() //CreateNew fuggveny letrehozasa
+        {
+            return new Ball(); //visszatérési érték
+        }
+    }
+}
