e5caec9724ea9d799a722e883a2e92dd0c638750
Attila Salamon <attila.salamon@stud.uni-corvinus.hu>
2020-11-15 15:11:32 +0100

    car

--- Christmas/Christmas/Entities/Ball.cs
+++ Christmas/Christmas/Entities/Ball.cs
@@ -5,33 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using Christmas.Abstractions;
 
 namespace Christmas.Entities
 {
-    public class Ball: Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
 
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g) //PROTECTED => ball osztály bármelyik leszármazottja használhatja 
+        protected override void DrawImage(Graphics g) //PROTECTED => ball osztály bármelyik leszármazottja használhatja 
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
-        {
-            Left += 1;
-        }
-    }
+    }   
 }
--- Christmas/Christmas/Form1.cs
+++ Christmas/Christmas/Form1.cs
@@ -43,7 +43,7 @@ namespace Christmas
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
