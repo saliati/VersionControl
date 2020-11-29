98ef34a2f0955ad3317eb7ff7d9ad311b110bec4
Attila Salamon <attila.salamon@stud.uni-corvinus.hu>
2020-11-29 22:45:28 +0100

    winner

--- EvolutionAI/EvolutionAI/Form1.cs
+++ EvolutionAI/EvolutionAI/Form1.cs
@@ -20,6 +20,8 @@ namespace EvolutionAI
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        Brain winnerBrain = null;
+
 
         public Form1()
         {
@@ -32,6 +34,7 @@ namespace EvolutionAI
             {
                 gc.AddPlayer(nbrOfSteps);
             }
+
             gc.Start();
 
             gc.GameOver += Gc_GameOver;
@@ -45,6 +48,39 @@ namespace EvolutionAI
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+            gc.Start();
         }
+
     }
 }
